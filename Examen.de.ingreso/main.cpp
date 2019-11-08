4251182950b08bd22c20bb931441d74b9700496b
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2019-11-08 13:54:43 -0500

    Examen de ingreso

--- Examen.de.ingreso/main.cpp
+++ Examen.de.ingreso/main.cpp
@@ -5,7 +5,7 @@
 #include <cstdlib>
 
 using namespace std;
-void ordenarburbuja(persona arr[],int tam){
+/*void ordenarburbuja(persona arr[],int tam){
      for(int i=0;i<tam;i++){
          for(int j=0;j<tam-1;j++){
             if(arr[j].obteneredad>arr[j+1].obteneredad){
@@ -15,10 +15,18 @@ void ordenarburbuja(persona arr[],int tam){
             }
          }
          }
+}*/
+void menu(){
+    cout<<"\n-----------Menu----------";//El menu que se utilizara en la base de datos
+    cout<<"\n 1. Revisar base de datos";
+    cout<<"\n 2. Nuevo ingresante";
+    cout<<"\n 3. Examen";
+    cout<<"\n 4. Salir";
+    cout<<"\nElija una opcion ===> ";
 }
 int main()
-{
-    int b,opc;
+{   int opc;
+    int b;
     int a;
     string f;
     string pl;
@@ -27,32 +35,34 @@ int main()
     persona p3(21,"Luciana",959875879,"Argentina");
     Examen e1(132,"computacion",34,"A",05,"carrera");
     persona arr1[]={p1,p2,p3};
-    cout<<"\n-----------Menu----------";//El menu que se utilizara en la base de datos
-    cout<<"\n 1. Revisar base de datos";
-    cout<<"\n 2. Nuevo ingresante";
-    cout<<"\n 3. Examen";
-    cout<<"\n 4. Salir";
-    cout<<"\nElija una opcion ===> ";
+    menu();
     cin>>opc;
     system("cls");
-    switch(opc){// Se usa la opcion switch para 4 opciones si se desea separar por partes se agrega system("cls");
-        case 1://opcion 1 para obtener los datos de la base
-            cout<<"Estudiantes ingresados:"<<endl;
-            cout<<p1.obteneredad()<<" "<<p1.obtenernombre()<<" "<<p1.obtenerDNI()<<" "<<p1.obtenerpais()<<endl;
-            cout<<p2.obteneredad()<<" "<<p2.obtenernombre()<<" "<<p2.obtenerDNI()<<" "<<p2.obtenerpais()<<endl;
-            cout<<p3.obteneredad()<<" "<<p3.obtenernombre()<<" "<<p3.obtenerDNI()<<" "<<p3.obtenerpais()<<endl;
-            break;
-            break;
-        case 2://opcion 2 para ingresar nuevos datos de un alumno
-            p2.personales();
-            break;
-        case 3://opcion 3 para obtener la clase del examen
-            e1.clase();
-            break;
-         case 4: exit(0);
-            break;
-         default : cout<<"Opcion no valida";
-    }
+    do{
+        switch(opc){// Se usa la opcion switch para 4 opciones si se desea separar por partes se agrega system("cls");
+            case 1://opcion 1 para obtener los datos de la base
+                cout<<"Estudiantes ingresados:"<<endl;
+                cout<<p1.obteneredad()<<" "<<p1.obtenernombre()<<" "<<p1.obtenerDNI()<<" "<<p1.obtenerpais()<<endl;
+                cout<<p2.obteneredad()<<" "<<p2.obtenernombre()<<" "<<p2.obtenerDNI()<<" "<<p2.obtenerpais()<<endl;
+                cout<<p3.obteneredad()<<" "<<p3.obtenernombre()<<" "<<p3.obtenerDNI()<<" "<<p3.obtenerpais()<<endl;
+                menu();
+                cin>>opc;
+                break;
+            case 2://opcion 2 para ingresar nuevos datos de un alumno
+                p2.personales();
+                menu();
+                cin>>opc;
+                break;
+            case 3://opcion 3 para obtener la clase del examen
+                e1.clase();
+                menu();
+                cin>>opc;
+                break;
+             case 4: exit(0);
+                break;
+             default : cout<<"Opcion no valida";
+        }
+    }while(opc!=4);
     system("pause");
         return 0;
 }
